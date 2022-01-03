fc9e1739a1794776f6cd14d8e654f993da942c8a
unknown <weamadel1994@gmail.com>
2022-01-03 08:00:11 +0200

    Add Tests for Lang Module
    Add tests for retrieving the correct language type.

--- lang/lang.ts
+++ lang/lang.ts
@@ -3,16 +3,19 @@ import InvalidLanguageType from "../errors/InvalidLanguageType";
 import AR_TRANSLATION from "./ar";
 import EN_TRANSLATION from "./en";
 
-enum Languages {
+export enum Languages {
   en = "en",
   ar = "ar",
 }
 
 const languages = {
-  en: AR_TRANSLATION,
-  ar: EN_TRANSLATION,
+  en: EN_TRANSLATION,
+  ar: AR_TRANSLATION,
 };
 
+/**
+ * Gets the the specified language.
+ */
 export function getLangTrans(lang: Languages = Languages.en) {
   if (!(lang in languages)) throw new InvalidLanguageType(lang);
 
