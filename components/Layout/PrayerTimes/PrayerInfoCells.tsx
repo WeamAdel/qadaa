d4055c0318a33833674af0f7989fd1a40ca775d3
unknown <weamadel1994@gmail.com>
2022-02-05 16:10:32 +0200

    Refactor Prayers Row & Cells Components

--- components/Layout/PrayerTimes/PrayerInfoCells.tsx
+++ components/Layout/PrayerTimes/PrayerInfoCells.tsx
@@ -1,13 +1,8 @@
-import { useContext } from "react";
-import { LangContext } from "../../../Providers/Language";
-
 import PrayerCell from "../../PrayersTable/PrayerCell";
 
 function PrayerInfoCells({ name, time }: { name: string; time: string }) {
-  const lang = useContext(LangContext);
   return (
     <>
-      <PrayerCell type="name">{lang[name]}</PrayerCell>
       <PrayerCell>{time}</PrayerCell>
     </>
   );
--- components/PrayersTable/PrayerRow.tsx
+++ components/PrayersTable/PrayerRow.tsx
@@ -1,5 +1,7 @@
-import { ReactNode } from "react";
+import { ReactNode, useContext } from "react";
+import { LangContext } from "../../Providers/Language";
 import Prayer from "../../types/Prayer";
+import PrayerCell from "./PrayerCell";
 
 import PrayerIcon from "./PrayerIcon";
 
@@ -11,9 +13,12 @@ interface PrayerRowInterface {
 }
 
 function PrayerRow({ prayerName, icon, children, ...props }: PrayerRowInterface) {
+  const lang = useContext(LangContext);
+
   return (
     <tr className={`prayers-table__row`} {...props}>
       <PrayerIcon name={prayerName}>{icon}</PrayerIcon>
+      <PrayerCell type="name">{lang[prayerName]}</PrayerCell>
       {children}
     </tr>
   );
