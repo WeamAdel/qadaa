fa41141a90fc5e2f6b7bf133ef43ecdef20e3000
unknown <weamadel1994@gmail.com>
2022-02-06 13:34:57 +0200

    Add By Count Form Max Prayer Count Validation

--- components/GenerateSchedule/ByCount/Form/Form.tsx
+++ components/GenerateSchedule/ByCount/Form/Form.tsx
@@ -1,21 +1,20 @@
-import { MutableRefObject, ReactNode, Ref, useContext, useRef, useState } from "react";
-import { useForm } from "react-hook-form";
+import { ReactNode, Ref, useContext, useState } from "react";
 import { LangContext } from "../../../../Providers/Language";
 import Prayer from "../../../../types/Prayer";
 
 import ErrorMessage from "../../../Form/ErrorMessage";
-import Label from "../../../Form/Label";
 import PrayersTable from "../../../PrayersTable/PrayersTable";
 import FormWrapper from "../../Tab/FormWrapper";
 import GenerateButton from "../../Tab/GenerateButton";
 import FormRowCells from "./FormRowCells";
 
-export const maxPrayerCount = 500;
+export const perPrayerMaxCount = 500;
 
 function Form() {
   //   const { numberOfYears, yearsRequired, minYears, maxYears } = useContext(LangContext);
   const [isEmpty, setIsEmpty] = useState(true);
   const [isTouched, setIsTouched] = useState(false);
+  const [maxCountExceeded, setMaxCountExceeded] = useState(false);
 
   function onSubmit(e: SubmitEvent) {
     e.preventDefault();
@@ -35,28 +34,34 @@ function Form() {
   function validate() {
     const inputs: NodeListOf<HTMLInputElement> = document.querySelectorAll(".prayer-count__input");
     let isEmpty = true;
+    let maxExceeded = false;
 
     for (let input of inputs) {
-      if (+input.value > 0) {
-        isEmpty = false;
-        break;
-      }
+      const value = +input.value;
+
+      isEmpty = value > 0 ? false : isEmpty;
+      maxExceeded = value > perPrayerMaxCount ? true : maxExceeded;
     }
 
     setIsEmpty(isEmpty);
+    setMaxCountExceeded(maxExceeded);
   }
 
-  let errorMessageJSX = <></>;
+  let errorMessages = [];
 
-  if (isTouched && isEmpty) {
-    //@ts-ignore
-    errorMessageJSX = <ErrorMessage message={"Please enter at leats one prayer count"} />;
+  if (isTouched) {
+    if (isEmpty) errorMessages.push("Please enter at leats one prayer count");
+    if (maxCountExceeded) errorMessages.push("Please enter numbers less than " + perPrayerMaxCount);
   }
+
+  const errorMessagesJSX = errorMessages.map((message, index) => {
+    return <ErrorMessage key={"by-count-error-" + index} message={message} />;
+  });
 
   return (
     <FormWrapper onSubmit={onSubmit}>
       <PrayersTable rowCells={getFormRowCells()}></PrayersTable>
-      {errorMessageJSX}
+      <>{errorMessagesJSX}</>
       <GenerateButton />
     </FormWrapper>
   );
