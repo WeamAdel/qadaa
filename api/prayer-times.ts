f6c560a4f0ba64f99f67371f47e60ac82e7bb9ed
unknown <weamadel1994@gmail.com>
2022-04-03 12:07:38 +0200

    Remove 'isomorphic-fetch' Package

--- api/prayer-times.ts
+++ api/prayer-times.ts
@@ -1,5 +1,3 @@
-require("isomorphic-fetch");
-
 export interface SuccessResponse {
   success: boolean;
   prayerTimes: {
--- next.config.js
+++ next.config.js
@@ -1,48 +1,44 @@
 /** @type {import('next').NextConfig} */
-import { ProvidePlugin } from "webpack";
 
-export const reactStrictMode = true;
-export const i18n = {
-  locales: ["en", "ar"],
-  defaultLocale: "en",
-  domains: [
-    {
-      domain: process.env.DOMAIN,
-      defaultLocale: "en",
-    },
-    {
-      domain: process.env.DOMAIN + "/ar",
-      defaultLocale: "ar",
-      http: true,
-    },
-  ],
-  trailingSlash: true,
-};
-export const trailingSlash = true;
-export function webpack(config) {
-  config.module.rules.push({
-    test: /\.svg$/,
-    use: [
+module.exports = {
+  reactStrictMode: true,
+  i18n: {
+    locales: ["en", "ar"],
+    defaultLocale: "en",
+    domains: [
       {
-        loader: "@svgr/webpack",
-        options: {
-          svgoConfig: {
-            plugins: [
-              {
-                name: "removeViewBox",
-                active: false,
-              },
-              new ProvidePlugin({
-                fetch: path.resolve(
-                  path.join(__dirname, "node_modules/isomorphic-fetch/fetch-npm-node")
-                ),
-              }),
-            ],
-          },
-        },
+        domain: process.env.DOMAIN,
+        defaultLocale: "en",
+      },
+      {
+        domain: process.env.DOMAIN + "/ar",
+        defaultLocale: "ar",
+        http: true,
       },
     ],
-  });
+    trailingSlash: true,
+  },
+  trailingSlash: true,
+  webpack: (config) => {
+    config.module.rules.push({
+      test: /\.svg$/,
+      use: [
+        {
+          loader: "@svgr/webpack",
+          options: {
+            svgoConfig: {
+              plugins: [
+                {
+                  name: "removeViewBox",
+                  active: false,
+                },
+              ],
+            },
+          },
+        },
+      ],
+    });
 
-  return config;
-}
+    return config;
+  },
+};
