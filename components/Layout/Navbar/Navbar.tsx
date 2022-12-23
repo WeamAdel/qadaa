f6ec1d682fd2c2bab7dfc1d0cdbc181772876e7a
unknown <weamadel1994@gmail.com>
2022-12-23 20:21:28 +0200

    Remove Unused Navbar Chevron Icon

--- components/Layout/Navbar/Navbar.tsx
+++ components/Layout/Navbar/Navbar.tsx
@@ -1,19 +1,11 @@
-import ChevronLeftIcon from "@mui/icons-material/ChevronLeft";
-import ChevronRightIcon from "@mui/icons-material/ChevronRight";
 import Logo from "./Logo";
-import { isRTLLang } from "../../../lang/lang";
 import NavItems from "./NavItems/NavItems";
 import OffCanvas from "./OffCanvas";
 import ToggleButton from "./ToggleButton";
 import { useState } from "react";
-import useMyRouter from "../../../hooks/useMyRouter";
 
 function Navbar() {
-  const { locale } = useMyRouter();
   const [isExpanded, setIspanded] = useState(false);
-
-  //@ts-ignore
-  const chevronIcon = isRTLLang(locale) ? <ChevronLeftIcon /> : <ChevronRightIcon />;
 
   function toggleMenu() {
     setIspanded((isExpanded) => {
